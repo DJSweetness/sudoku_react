f3fea1c7bc088cdfba5ba7401d4acd842429940b
djsweetness <david.simoneau@promoonly.com>
2018-04-03 14:05:26 -0400

    check board is complete and adds buttons for getMistake and checkBoardResult

--- src/SudokuGame.js
+++ src/SudokuGame.js
@@ -4,6 +4,114 @@ import logo from './logo.svg';
 import './SudokuGame.css';
 
 
+function checkBoard(grid) 
+{
+
+    let i,j,k,l;
+    let rowsChecked = Array(9).fill(0);
+    let colsChecked = Array(9).fill(0);
+    let blocksChecked = Array(9).fill(0);
+
+    // ~ Check blocks ~ //
+    // for each block...
+    for(i=0;i<9;i++)
+    {
+      // for each value in block...
+      for(j=0;j<9;j++)
+      {
+        //set corresponding index of number to 1, meaning we have that number
+        //We subtract by 1 because our indexes are 0-8 instead of the numbers
+        //in sudoku which are 1-9.
+        blocksChecked[grid[i][j]-1] = 1;
+      }
+      
+      // if a 0, then it's failed the test already.
+      if (blocksChecked.includes(0)) {
+        return ['Error block', i];
+      }
+      
+      blocksChecked.fill(0);
+    }
+    
+    // ~ Check rows ~ //
+    let row = 0;
+    for(i=0;i<3;i++)
+    {
+      for(j=0;j<3;j++)
+      {
+        for(k=0;k<3;k++)
+        {
+          for(l=0;l<3;l++)
+          {
+            rowsChecked[ grid[k+(i*3)][l+(j*3)] - 1 ] = 1; 
+          }
+        }
+        
+        if (rowsChecked.includes(0)) {
+          return ['Error row', row, rowsChecked];
+        }
+        row++;
+        rowsChecked.fill(0);
+        
+      }
+    }
+    
+    // ~ Check columns ~ //
+    let col = 0;
+    for(i=0;i<3;i++)
+    {
+      for(j=0;j<3;j++)
+      {
+        for(k=0;k<3;k++)
+        {
+          for(l=0;l<3;l++)
+          {
+            colsChecked[ grid[k*3+i][l*3+j] - 1 ] = 1;
+          }
+        }
+        
+        // if a 0, then it's failed the test already.
+        if (colsChecked.includes(0)) {
+          return ['Error col', col];
+        }
+        col++;
+        colsChecked.fill(0);
+        
+      }
+    }
+    
+    return Array('You Win!');
+}
+
+
+function getMistake(checkBoardResult)
+{
+
+  if (checkBoardResult[0] === 'Error block')
+  {
+    console.log('Error block');
+  }
+  else if (checkBoardResult[0] === 'Error row')
+  {
+    console.log('Error row');
+  }
+  else if (checkBoardResult[0] === 'Error col')
+  {
+    console.log('Error col');
+  }
+  else if (checkBoardResult[0] === 'You Win!')// winner
+  {
+
+  }
+
+}
+
+
+function checkIfSolved(checkBoardResult)
+{
+
+}
+
 class Square extends Component {
 
   render() {
@@ -21,11 +129,10 @@ class Square extends Component {
 
 class BoxOfSquares extends Component {
 
-  renderSquare(i, j, e) {
+  renderSquare(i, j) {
     return (
       <Square className='square' 
               onInput={this.props.onInput}
-              grid={this.props.grid}
               value_i = {i}
               value_j = {j}
       />
@@ -62,101 +169,20 @@ class BoxOfSquares extends Component {
 
 }
 
-function checkBoard(grid) 
-{
-
-    let i,j,k,l;
-    let rowsChecked = Array(9).fill(0);
-    let colsChecked = Array(9).fill(0);
-    let blocksChecked = Array(9).fill(0);
-    
-    let numBlocks = 9;
-
-    // // ~ Check blocks ~ //
-    // // for each block...
-    // for(i=0;i<9;i++)
-    // {
-    //   // for each value in block...
-    //   for(j=0;j<9;j++)
-    //   {
-    //     //set corresponding index of number to 1, meaning we have that number
-    //     //We subtract by 1 because our indexes are 0-8 instead of the numbers
-    //     //in sudoku which are 1-9.
-    //     blocksChecked[grid[i][j]-1] = 1;
-    //   }
-      
-    //   // if a 0, then it's failed the test already.
-    //   if (blocksChecked.includes(0)) {
-    //     return Array('Error block', i);
-    //   }
-      
-    //   blocksChecked.fill(0);
-    // }
-    
-    // ~ Check rows ~ //
-    let row = 0;
-    for(i=0;i<3;i++)
-    {
-      for(j=0;j<3;j++)
-      {
-        for(k=0;k<3;k++)
-        {
-          for(l=0;l<3;l++)
-          {
-            if (rowsChecked[ grid[k+(i*3)][l+(j*3)] - 1 ] == 1)
-            {
-              return Array(k, i*3, l, j*3);
-            }
-            else
-            {
-              rowsChecked[ grid[k+(i*3)][l+(j*3)] - 1 ] = 1; 
-            }
-          }
-        }
-        
-        if (rowsChecked.includes(0)) {
-          return Array('Error block', row, rowsChecked);
-        }
-        row++;
-        rowsChecked.fill(0);
-        
-      }
-    }
-    
-    // ~ Check columns ~ //
-    for(i=0;i<3;i++)
-    {
-      for(j=0;j<3;j++)
-      {
-        for(k=0;k<3;k++)
-        {
-          for(l=0;l<3;l++)
-          {
-            colsChecked[ grid[k*3+i][l*3+j] - 1 ] = 1;
-          }
-        }
-        
-        // if a 0, then it's failed the test already.
-        if (colsChecked.includes(0)) {
-          return Array('Error col', i, j);
-        }
-        
-        colsChecked.fill(0);
-        
-      }
-    }
-    
-    return Array('You Win!');
-}
-
 
 class Board extends Component {
 
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
+    let grid = [];
+    for(let i=0; i<9; i++)
+    {
+      grid[i] = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+    }
     this.state = {
-      grid: Array(9).fill(Array(9).fill(-1)),
+      grid: grid,
+      checkBoardResult: '',
       value: ''
     };
   }
@@ -165,40 +191,46 @@ class Board extends Component {
     return(
       <BoxOfSquares 
         onInput={this.handleInputChange}
-        grid={this.state.grid}
         value={i}
       />
     );
   }
 
   handleInputChange = (i,j,e) => {
-    
-    this.state.grid[i][j] = e.target.value;
-    console.log(checkBoard(this.state.grid));
+
+    const grid_temp = this.state.grid.slice();
+    grid_temp[i][j] = e.target.value;
+    this.setState({grid: grid_temp});
+
+    this.setState({checkBoardResult: checkBoard(this.state.grid)});
 
   }
 
   render(){
     
     return (
-      <div className='board'>
-        <div className='boxOfSquares-row'>
-          {this.renderBoxOfSquares(0)}
-          {this.renderBoxOfSquares(1)}
-          {this.renderBoxOfSquares(2)}
-        </div>
+      <div>
+        <div className='board'>
+          <div className='boxOfSquares-row'>
+            {this.renderBoxOfSquares(0)}
+            {this.renderBoxOfSquares(1)}
+            {this.renderBoxOfSquares(2)}
+          </div>
 
-        <div className='boxOfSquares-row'>
-          {this.renderBoxOfSquares(3)}
-          {this.renderBoxOfSquares(4)}
-          {this.renderBoxOfSquares(5)}
-        </div>
+          <div className='boxOfSquares-row'>
+            {this.renderBoxOfSquares(3)}
+            {this.renderBoxOfSquares(4)}
+            {this.renderBoxOfSquares(5)}
+          </div>
 
-        <div className='boxOfSquares-row'>
-          {this.renderBoxOfSquares(6)}
-          {this.renderBoxOfSquares(7)}
-          {this.renderBoxOfSquares(8)}
+          <div className='boxOfSquares-row'>
+            {this.renderBoxOfSquares(6)}
+            {this.renderBoxOfSquares(7)}
+            {this.renderBoxOfSquares(8)}
+          </div>
         </div>
+        <button class='getMistake' >Get Mistake</button>
+        <button class='checkBoard' >Check Solution</button>
       </div>
 
     );
